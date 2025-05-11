c5f14170930c7b5aaef81dd436f59e163784f9e2
RomainJlt <romainjollivet0@gmail.com>
2025-05-11 11:21:26 +0200

    pickUp alcool

--- Assets/Settings/script/Alcool/AlcoolManager.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class AlcoolManager : MonoBehaviour
-{
-    public int alcoolcount = 0;
-}
--- Assets/Settings/script/Alcool/PickUpAlcool.cs
+++ Assets/Settings/script/Alcool/PickUpAlcool.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class PickUpAlcool : MonoBehaviour
 {
-    public AlcoolManager am;
+    // public AlcoolManager am; // glisse ici ton GameObject avec le script AlcoolManager dans l'inspecteur
+
+    public int alcoolcount = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")){
-
-            // Détruire l'objet alcool
+        if (collision.CompareTag("Player"))
+        {
+            Inventory.instance.AddAlcool(1); // Ajoute l'alcool à l'inventaire du joueur
             Destroy(gameObject);
         }
     }
 }
+
